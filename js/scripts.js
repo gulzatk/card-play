5cc5397ab911b935b5d7fab77b4b7c8f10c34e3c
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-10-23 23:04:01 -0700

    it will updated the file using for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 
-$(document).ready(function(event) {
+$(document).ready(function() {
   // event.preventDefault();
 
   var suits = ["diamonds", "spades", "hearts", "clubs"]
   var values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, "Jack", "Queen", "King"]
-
-
- values.forEach(function(value){
-  suits.forEach(function(suit) {
-    $(".list").text(suit + "of" + value);
-
-
+var cards = [];
+// for (i = 0; i < suits.length; i ++){
+//   for (j = 0; j < values.length; j ++){
+//    $(".list").append("<li>" + suits[i] + "of" + values[j] + "</li>");
+//   }
+// }
+  suits.forEach(function(suit){
+   values.forEach(function(value){
+    $(".list").append("<li>" + suit + "of" + value + "</li>")
   });
 });
+
 
 
   });
